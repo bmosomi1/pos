8faab676c84a77a3c1d92fe35eaa7d8818062e72
Benard Mosomi <benardmosomi@gmail.com>
2023-05-31 13:32:44 +0300

    readers

--- django_point_of_sale-main/django_pos/payments/models.py
+++ django_point_of_sale-main/django_pos/payments/models.py
@@ -4,7 +4,7 @@ from customers.models import Customer
 from products.models import Product
 
 
-class Sale(models.Model):
+class Payments(models.Model):
     date_added = models.DateTimeField(default=django.utils.timezone.now)
     customer = models.ForeignKey(
         Customer, models.DO_NOTHING, db_column='customer')
@@ -16,7 +16,7 @@ class Sale(models.Model):
     amount_change = models.FloatField(default=0)
 
     class Meta:
-        db_table = 'Sales'
+        db_table = 'payments'
 
     def __str__(self) -> str:
         return "Sale ID: " + str(self.id) + " | Grand Total: " + str(self.grand_total) + " | Datetime: " + str(self.date_added)
@@ -26,9 +26,9 @@ class Sale(models.Model):
         return sum([d.quantity for d in details])
 
 
-class SaleDetail(models.Model):
-    sale = models.ForeignKey(
-        Sale, models.DO_NOTHING, db_column='sale')
+class PaymentDetail(models.Model):
+    payment = models.ForeignKey(
+        Payment, models.DO_NOTHING, db_column='sale')
     product = models.ForeignKey(
         Product, models.DO_NOTHING, db_column='product')
     price = models.FloatField()
@@ -36,7 +36,7 @@ class SaleDetail(models.Model):
     total_detail = models.FloatField()
 
     class Meta:
-        db_table = 'SaleDetails'
+        db_table = 'PaymentDetails'
 
     def __str__(self) -> str:
         return "Detail ID: " + str(self.id) + " Sale ID: " + str(self.sale.id) + " Quantity: " + str(self.quantity)
