db6b80c48f39a200cfe8cd69db498283f59d9b2b
Benard Mosomi <benardmosomi@gmail.com>
2023-05-31 16:18:29 +0300

    readers

--- django_point_of_sale-main/django_pos/products/views.py
+++ django_point_of_sale-main/django_pos/products/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from .models import Category, Product
+from .models import Category, Product, Order
 
 
 @login_required(login_url="/accounts/login/")
